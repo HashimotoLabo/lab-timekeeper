b5429bb1e3c6e69eecc191c51e4cf406a867cb73
HASHIMOTOLABPC\eicha <eichan216@gmail.com>
2025-05-04 16:32:12 +0900

    add : ログインカード作成

--- lab-timekeeper/src/features/login/pages/LoginPage.vue
+++ lab-timekeeper/src/features/login/pages/LoginPage.vue
@@ -1,5 +1,20 @@
 <template>
-ログインページ
+<v-container>
+<v-row>
+  <v-col>
+    <v-card>
+      <v-card-title>Login</v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Email" v-model="email" />
+          <v-text-field label="Password" type="password" v-model="password" />
+          <v-btn @click="login">Login</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-col>
+</v-row>
+</v-container>
 </template>
 
 <script setup lang="ts">
