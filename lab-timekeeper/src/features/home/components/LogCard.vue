213d3a421fe4bada5b4f7d6bf65ce2ee96bf1392
DESKTOP-65O4IAG\hlab3902 <a21.bkr5@g.chuo-u.ac.jp>
2025-05-15 16:31:41 +0900

    fix:スマホ表示に対応

--- lab-timekeeper/src/features/home/components/LogCard.vue
+++ lab-timekeeper/src/features/home/components/LogCard.vue
@@ -2,11 +2,22 @@
   <v-card>
     <v-card-title class="text-h5">ログ</v-card-title>
     <v-card-text>
-      <v-data-table :items="logs"></v-data-table>
+      <v-data-table :items="logs" hide-default-header v-if="$vuetify.display.smAndDown">
+        
+        <template #item="{ item }">
+
+            <p><strong>createdAt</strong> {{ item.createdAt }}</p>
+            <p><strong>memo</strong>   {{ item.memo }}</p>
+            <p> <strong>state</strong>  {{ item.state }}</p>
+            <p> <strong>uid</strong>  {{ item.uid }}</p>
+            <v-divider></v-divider>
+        </template>
+      </v-data-table>
+      <v-data-table :items="logs" v-else>
+      </v-data-table>
     </v-card-text>
   </v-card>
 </template>
-
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 
